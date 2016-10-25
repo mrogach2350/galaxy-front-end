cd7c82ec3b3b6e0019e07abedc97d4fb7236c640
mrogach2350 <m.rogach@gmail.com>
2016-10-25 16:18:35 -0700

    adds prompt text

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ $(document).ready(function() {
   // triggers login modal
   $('.modal-trigger').leanModal();
 
+  $(".dropdown-button").dropdown();
+
   // hides login and unhides logout button on login
   $('.modal-action').on('click', function(){
     $('#login').addClass('hidden');
+    $('.out-banner').addClass('hidden');
     $('#logout').removeClass('hidden');
+    $('.in-banner').removeClass('hidden');
   });
 
   // hides logout and reveals login on logout
   $('#logout').on('click', function(){
     $('#login').removeClass('hidden');
+    $('.out-banner').removeClass('hidden');
     $('#logout').addClass('hidden');
+    $('.in-banner').addClass('hidden');
   });
 });
